a064cfcb2685ca0c1c2f8f60eef14560b083a2fa
adriangrunder <74954532+adriangrunder@users.noreply.github.com>
2021-03-10 13:08:43 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1IhxBmR5fWJmMu95pk1emJXPpWE7-7SSoRGU8kQAhksk/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyBGpQvD3cSUyPIwt-FY2-rbvP0pxs_ITeo';
